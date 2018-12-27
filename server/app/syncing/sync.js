753aac9e28465c715f453f215a72d37f161b451c
ZachMoore <Get3Moore@gmail.com>
2018-12-27 12:53:07 -0500

    Add continous realtime data updating

--- server/app/syncing/sync.js
+++ server/app/syncing/sync.js
@@ -1,9 +1,23 @@
 const fsSync = require("./lib/fsSync.js")
+const dbMethods = require("../lib/database/methods/index.js")
+const responseHandler = require("../lib/fetch/responseHandler.js")
 
 
 fsSync.login()
 .then((data)=>{
     console.log("Kiln logged in:", data)
+
+    setInterval(()=>{
+
+            dbMethods.kiln.updateRealtimeData()
+        .then(responseHandler)
+        .then(data=>{
+            console.log(data)
+        })
+        .catch((console.log))
+
+    }, 10*1000)
+
 })
 .catch((data)=>{
     console.log("There was an error logging in the kiln:", data)
