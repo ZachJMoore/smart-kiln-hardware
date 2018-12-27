75c72fa69573415e0550d76d3223593c1f0ce838
ZachMoore <Get3Moore@gmail.com>
2018-12-27 12:11:27 -0500

    Add function for updating a kilns realtime data row on the database

--- server/app/lib/database/methods/kiln.js
+++ server/app/lib/database/methods/kiln.js
@@ -1,11 +1,12 @@
 const fetch = require("../../fetch/fetch.js")
+const kiln = require("../../kiln.js")
 
 class Kiln {
 
     constructor(config){
 
         this.signUp = (password) => {
-            return fetch(`${config.databaseHost}/api/kiln/sign-up`, {
+            return fetch(`${config.databaseHost}/api/private/kiln/sign-up`, {
                 ...config.requestConfig.kiln,
                 method: "post",
                 body: JSON.stringify({
@@ -15,7 +16,7 @@ class Kiln {
         }
 
         this.login = (password, uuid) => {
-            return fetch(`${config.databaseHost}/api/kiln/login`, {
+            return fetch(`${config.databaseHost}/api/private/kiln/login`, {
                 ...config.requestConfig.kiln,
                 method: "post",
                 body: JSON.stringify({
@@ -27,13 +28,33 @@ class Kiln {
 
         this.getKilnData = () => {
 
-            return fetch(`${config.databaseHost}/api/kiln/get-kiln-data`, ...config.requestConfig.kiln)
+            return fetch(`${config.databaseHost}/api/private/kiln/get-kiln-data`, ...config.requestConfig.kiln)
 
         }
 
         this.getAllSchedules = (userId = 0)=>{
 
-            return fetch(`${config.databaseHost}/api/kiln/get-all-schedules`, ...config.requestConfig.kiln)
+            return fetch(`${config.databaseHost}/api/private/kiln/get-all-schedules`, ...config.requestConfig.kiln)
+
+        }
+
+        this.updateRealtimeData = () => {
+
+            let kilnData = kiln.getPackage()
+
+            let data = {
+                is_firing: kilnData.isFiring,
+                firing_schedule_id: kilnData.currentSchedule.id,
+                current_temperature: kilnData.temperature,
+                estimated_minutes_remaining: kilnData.estimatedMinutesRemaining
+            }
+
+
+            return fetch(`${config.databaseHost}/api/private/kiln/update-realtime-data`, {
+                ...config.requestConfig.kiln,
+                method: "post",
+                body: JSON.stringify(data)
+            })
 
         }
 
