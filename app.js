9486b004ac9c3e3252de6137aeb271f3dfddce81
Zach Moore <get3moore@gmail.com>
2019-03-21 10:13:14 -0400

    Add startup console log

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
-require("dotenv").config()
-const passel = require("passeljs")
-const Kiln = require("./app/components/Kiln")
-const Authentication = require("./app/components/Authentication")
-const Schedules = require("./app/components/Schedules")
-const FiringLogger = require("./app/components/FiringLogger")
-const DatapointLogger = require("./app/components/DatapointLogger")
-const CommandRunner = require("./app/components/CommandRunner")
-const ZeroConf = require("./app/components/ZeroConf")
-const RealtimeData = require("./app/components/RealtimeData")
-const WifiManager = require("./app/components/WifiManager")
-const io = require("socket.io")(8009)
-
+require("dotenv").config();
+const passel = require("passeljs");
+const Kiln = require("./app/components/Kiln");
+const Authentication = require("./app/components/Authentication");
+const Schedules = require("./app/components/Schedules");
+const FiringLogger = require("./app/components/FiringLogger");
+const DatapointLogger = require("./app/components/DatapointLogger");
+const CommandRunner = require("./app/components/CommandRunner");
+const ZeroConf = require("./app/components/ZeroConf");
+const RealtimeData = require("./app/components/RealtimeData");
+const WifiManager = require("./app/components/WifiManager");
+const io = require("socket.io")(8009);
 
 // Start application
 passel.setGlobalDefaults({
-    socket: null,
-    io
-})
+  socket: null,
+  io
+});
 
-passel.use(Authentication)
-passel.use(Kiln)
-passel.use(Schedules)
-passel.use(FiringLogger)
-passel.use(DatapointLogger)
-passel.use(CommandRunner)
-passel.use(ZeroConf)
-passel.use(RealtimeData)
-passel.use(WifiManager)
+passel.use(Authentication);
+passel.use(Kiln);
+passel.use(Schedules);
+passel.use(FiringLogger);
+passel.use(DatapointLogger);
+passel.use(CommandRunner);
+passel.use(ZeroConf);
+passel.use(RealtimeData);
+passel.use(WifiManager);
 
-passel.mountComponents()
+passel.mountComponents();
 
+console.log(new Date() + ": application started");
 
 // Testing memory usage of application
 // setInterval(()=>{
