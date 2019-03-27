194a204ab2c9711f0832aa42efb94e80b3b37181
Zach Moore <get3moore@gmail.com>
2019-03-27 19:29:21 -0400

    Change zeroconf default name

--- app/components/Authentication/lib/Socket/index.js
+++ app/components/Authentication/lib/Socket/index.js
@@ -1,79 +1,72 @@
-const { Components } = require("passeljs")
-const io = require("socket.io-client")
+const { Components } = require("passeljs");
+const io = require("socket.io-client");
 
-module.exports = class Socket extends Components.Base{
-    constructor(props){
-        super(props)
+module.exports = class Socket extends Components.Base {
+  constructor(props) {
+    super(props);
 
-        this.componentName = "Socket"
+    this.componentName = "Socket";
 
-        this.state = {
-            isConnected: false,
-            authenticationError: null,
-        }
+    this.state = {
+      isConnected: false,
+      authenticationError: null
+    };
 
-        this.socket = null
-        this.reconnectInterval = null
-    }
+    this.socket = null;
+    this.reconnectInterval = null;
+  }
 
-    componentWillMount(){
+  componentWillMount() {
+    this.parentStateChanged.on("httpIsAuthenticated", httpIsAuthenticated => {
+      if (httpIsAuthenticated && !this.parentState.socketIsAuthenticated) {
+        clearInterval(this.reconnectInterval);
+        this.socket.connect();
+      } else {
+      }
+    });
 
-        this.parentStateChanged.on("httpIsAuthenticated", (httpIsAuthenticated)=>{
-            if (httpIsAuthenticated && !this.parentState.socketIsAuthenticated){
-                clearInterval(this.reconnectInterval)
-                this.socket.connect()
-            } else {
+    this.socket = io(process.env.DB_HOST + "/kiln");
 
-            }
-        })
+    this.socket.on("connect", () => {
+      this.setState({ isConnected: true });
+      this.socket.emit("authentication", this.parentState.credentials);
+    });
 
-        this.socket = io(process.env.DB_HOST + "/kiln")
+    this.socket.on("authenticated", () => {
+      this.props.updateAuthState(true);
+      this.socket.emit("get-account-data", error => {
+        if (error) console.log(error);
+      });
+    });
 
-        this.socket.on("connect", ()=>{
+    this.socket.on("unauthorized", error => {
+      this.setState({
+        authenticationError: error
+      });
+      this.socket.disconnect();
+    });
 
-            this.setState({isConnected: true})
-            this.socket.emit("authentication", this.parentState.credentials)
+    this.socket.on("disconnect", () => {
+      this.props.updateAuthState(false);
+      this.setState({
+        isConnected: false
+      });
 
-        })
+      clearInterval(this.reconnectInterval);
+      this.reconnectInterval = setInterval(() => {
+        if (this.state.isConnected) clearInterval(this.reconnectInterval);
+        else this.socket.connect();
+      }, (process.env.SOCKET_RECONNECT_ATTEMPT_INTERVAL_SECONDS || 10) * 1000);
+    });
 
-        this.socket.on("authenticated", ()=>{
-            this.props.updateAuthState(true)
-            this.socket.emit("get-account-data", (error)=>{
-                if (error) console.log(error)
-            })
-        })
+    this.setGlobal({
+      socket: this.socket
+    });
+  }
 
-        this.socket.on("unauthorized", (error)=>{
-            this.setState({
-                authenticationError: error
-            })
-            this.socket.disconnect()
-        })
-
-        this.socket.on("disconnect", ()=>{
-            this.props.updateAuthState(false)
-            this.setState({
-                isConnected: false,
-            })
-
-            clearInterval(this.reconnectInterval)
-            this.reconnectInterval = setInterval(() => {
-                if (this.state.isConnected) clearInterval(this.reconnectInterval)
-                this.socket.connect()
-            }, (process.env.SOCKET_RECONNECT_ATTEMPT_INTERVAL_SECONDS || 10) * 1000);
-        })
-
-        this.setGlobal({
-            socket: this.socket
-        })
-    }
-
-
-    componentDidMount(){
-
-        this.socket.on("account-data", (data)=>{
-            this.props.updateAccountData(data)
-        })
-
-    }
-}
+  componentDidMount() {
+    this.socket.on("account-data", data => {
+      this.props.updateAccountData(data);
+    });
+  }
+};
