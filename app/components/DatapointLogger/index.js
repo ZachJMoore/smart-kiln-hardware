e911b1f0d8ae704750a5d4bd597b8b364287ec7f
Zach Moore <get3moore@gmail.com>
2019-09-06 18:00:02 -0400

    Make adjustments to intergrate graphing temps

--- app/components/DatapointLogger/index.js
+++ app/components/DatapointLogger/index.js
@@ -1,123 +1,137 @@
 const { Components } = require("passeljs");
 
-module.exports = class DatapointLogger extends Components.Base{
-    constructor(props){
-        super(props)
+module.exports = class DatapointLogger extends Components.Base {
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            datapoints: [],
-            backupDatapoints: [],
-        }
+    this.state = {
+      datapoints: [],
+      backupDatapoints: []
+    };
 
-        this.options = {
-            fsState: {
-                recurrentUpdateLimit: (process.env.DATAPOINT_FS_INTERVAL_LIMIT_SECONDS || 600) * 1000,
-                options: {
-                    include: [
-                        {
-                            key: "datapoints"
-                        },
-                        {
-                            key: "backupDatapoints"
-                        }
-                    ]
-                }
+    this.options = {
+      fsState: {
+        recurrentUpdateLimit:
+          (process.env.DATAPOINT_FS_INTERVAL_LIMIT_SECONDS || 600) * 1000,
+        options: {
+          include: [
+            {
+              key: "datapoints"
             },
-            globalState:{
-                options: {
-                    include: [
-                        {
-                            key: "datapoints",
-                            emit: true
-                        }
-                    ]
-                }
+            {
+              key: "backupDatapoints"
             }
+          ]
         }
+      },
+      globalState: {
+        options: {
+          include: [
+            {
+              key: "datapoints",
+              emit: true
+            }
+          ]
+        }
+      }
+    };
+  }
+
+  getDatapoint() {
+    return {
+      temperature: this.global.Kiln.thermoSensor.average,
+      created_at: Date.now()
+    };
+  }
+
+  trimDatapoints(datapoints, isBackup = false) {
+    let limit = null;
+
+    if (isBackup) {
+      limit = process.env.DATAPOINT_BACKUP_LIMIT_COUNT;
+      limit = parseInt(limit);
+      if (isNaN(limit)) limit = 720;
+    } else {
+      limit = process.env.DATAPOINT_LIMIT_COUNT;
+      limit = parseInt(limit);
+      if (isNaN(limit)) limit = 360;
     }
 
-    getDatapoint(){
-        return {
-            temperature: this.global.Kiln.thermoSensor.average,
-            created_at: Date.now()
-        }
+    if (datapoints.length > limit) {
+      let sliceCount = datapoints.length - limit;
+      datapoints.splice(0, sliceCount);
     }
+  }
 
-    trimDatapoints(datapoints, isBackup = false){
+  trackDatapoints() {
+    let datapoint = this.getDatapoint();
 
-        let limit = null
+    let datapoints = [...this.state.datapoints, datapoint];
+    this.trimDatapoints(datapoints);
 
-        if (isBackup) {
-            limit = process.env.DATAPOINT_BACKUP_LIMIT_COUNT
-            limit = parseInt(limit)
-            if (isNaN(limit)) limit = 720
-        } else { 
-            limit = process.env.DATAPOINT_LIMIT_COUNT
-            limit = parseInt(limit)
-            if (isNaN(limit)) limit = 360
+    let backupDatapoints = [...this.state.backupDatapoints, datapoint];
+    this.trimDatapoints(backupDatapoints, true);
+
+    this.global.socket.emit(
+      "kiln-to-user-temperature-datapoints-update",
+      datapoints
+    );
+
+    if (!this.global.Authentication.socketIsAuthenticated) {
+      // If we aren't authenticated: backup datapoints
+      this.setState({
+        datapoints,
+        backupDatapoints
+      });
+    } else {
+      this.global.socket.emit(
+        "bulk-add-temperature-datapoints",
+        backupDatapoints,
+        error => {
+          if (error) {
+            throw new Error(error.message);
+          } else {
+            backupDatapoints = [];
+          }
+
+          this.setState({
+            datapoints,
+            backupDatapoints
+          });
+          if (!error) this.ensureFsState();
         }
-
-        if (datapoints.length > limit){
-            let sliceCount = datapoints.length - limit
-            datapoints.splice(0, sliceCount)
-        }
+      );
     }
+  }
 
-    trackDatapoints(){
-        let datapoint = this.getDatapoint()
-
-        let datapoints = [...this.state.datapoints, datapoint]
-        this.trimDatapoints(datapoints)
-
-        let backupDatapoints = [...this.state.backupDatapoints, datapoint]
-        this.trimDatapoints(backupDatapoints, true)
-
-        if (!this.global.Authentication.socketIsAuthenticated){
-
-            // If we aren't authenticated: backup datapoints
-            this.setState({
-                datapoints,
-                backupDatapoints
-            })
-
-        } else {
-            this.global.socket.emit("bulk-add-temperature-datapoints", backupDatapoints, (error)=>{
-                if (error){
-                    throw new Error(error.message)
-                } else {
-                    backupDatapoints = []
-                }
-
+  componentWillMount() {
+    this.globalChanged.on(
+      "Authentication.socketIsAuthenticated",
+      socketIsAuthenticated => {
+        if (socketIsAuthenticated) {
+          this.global.socket.emit(
+            "bulk-add-temperature-datapoints",
+            this.state.backupDatapoints,
+            error => {
+              if (error) {
+                throw new Error(error.message);
+              } else {
                 this.setState({
-                    datapoints,
-                    backupDatapoints
-                })
-                if (!error) this.ensureFsState()
-            })
-        }
-    }
-
-    componentWillMount(){
-        this.globalChanged.on("Authentication.socketIsAuthenticated", (socketIsAuthenticated)=>{
-            if (socketIsAuthenticated){
-                this.global.socket.emit("bulk-add-temperature-datapoints", this.state.backupDatapoints, (error)=>{
-                    if (error){
-                        throw new Error(error.message)
-                    } else {
-                        this.setState({
-                            backupDatapoints: []
-                        })
-                        this.ensureFsState()
-                    }
-                })
+                  backupDatapoints: []
+                });
+                this.ensureFsState();
+              }
             }
-        })
-    }
+          );
+        }
+      }
+    );
+  }
 
-    componentDidMount(){
-        this.trackDatapoints()
-        setInterval(()=>{
-            this.trackDatapoints()
-        }, (process.env.DATAPOINT_UPDATE_INTERVAL_SECONDS || 60) * 1000)
-    }
-}
+  componentDidMount() {
+    this.trackDatapoints();
+    setInterval(() => {
+      this.trackDatapoints();
+    }, (process.env.DATAPOINT_UPDATE_INTERVAL_SECONDS || 60) * 1000);
+  }
+};
